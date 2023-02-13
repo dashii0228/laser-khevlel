696cae90ea063d4e2419160c6024ae5861dd2aff
Dashii <125039213+dashii0228@users.noreply.github.com>
2023-02-13 23:59:08 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ const IndexPage = () => {
         maxWidth={'500px'}
         image={'/banner1.png'}
         title={'ЛАЗЕР ХЭВЛЭЛИЙН ҮЙЛЧИЛГЭЭ'}
-        subtitle={'Discover Autumn Winter 2021'}
+        subtitle={'Бүх төрлийн материал дээрх хэвлэл'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
